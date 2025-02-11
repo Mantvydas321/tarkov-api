34919510d2f7aa46298f600c61a60947c51500b5
Mantvydas321 <farozen.tv@gmail.com>
2025-02-11 16:35:39 +0100

    Add database connection and entities Add databasecontext Add achievement entity Add achievement entityconfig

--- tarkov-api/Database/DatabaseContext.cs
+++ tarkov-api/Database/DatabaseContext.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using tarkov_api.Database.Entities;
+
 namespace tarkov_api.Database;
 
-public class DatabaseContext
+public class DatabaseContext : DbContext
 {
+    public DbSet<AchievmentEntity> Achievments { get; set; }
     
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
 }
--- tarkov-api/Program.cs
+++ tarkov-api/Program.cs
@@ -1,12 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using tarkov_api.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add database context
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Apply pending migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DatabaseContext>();
+    context.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
